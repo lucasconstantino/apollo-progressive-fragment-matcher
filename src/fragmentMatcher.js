717630b0a47c931d1bbc340264d4c8fc79aab9fb
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-15 15:17:09 -0300

    feat: initiated strategy differentiation for ProgressiveFragmentMatcher

--- src/fragmentMatcher.js
+++ src/fragmentMatcher.js
@@ -1,34 +1,63 @@
 import { ApolloLink } from 'apollo-link'
 import invariant from 'invariant'
 
-export class ProgressiveFragmentMatcher {
-  constructor () {
-    // initiate type map.
-    this.possibleTypesMap = {}
+const defaultOptions = {
+  strategy: 'extension' // 'extension' | ''
+}
 
-    // necessary bindings.
-    this.match = this.match.bind(this)
-  }
+const strategies = {
+  /**
+   * Uses GraphQL extensions to share possibleTypes from server to client.
+   *
+   * Upsides: fast.
+   * Downsides: requires server contol (add extension).
+   */
+  extension: {
+    link () {
+      return new ApolloLink((operation, forward) => {
+        // enable possible types fetching.
+        operation.extensions.possibleTypes = true
 
-  link () {
-    return new ApolloLink((operation, forward) => {
-      // enable possible types fetching.
-      operation.extensions.possibleTypes = true
+        return forward(operation).map(result => {
+          if (result.extensions && result.extensions.possibleTypes) {
+            const types = result.extensions.possibleTypes
 
-      return forward(operation).map(result => {
-        if (result.extensions && result.extensions.possibleTypes) {
-          const types = result.extensions.possibleTypes
-
-          for (let type in types) {
-            if (types.hasOwnProperty(type) && !this.possibleTypesMap[type]) {
-              this.possibleTypesMap[type] = types[type]
+            for (let type in types) {
+              if (types.hasOwnProperty(type) && !this.possibleTypesMap[type]) {
+                this.possibleTypesMap[type] = types[type]
+              }
             }
           }
-        }
 
-        return result
+          return result
+        })
       })
-    })
+    }
+  }
+}
+
+export class ProgressiveFragmentMatcher {
+  constructor (options) {
+    const { strategy: name } = { ...defaultOptions, ...options }
+
+    if (!strategies[name]) {
+      const strategyNames = Object.keys(strategies)
+        .map(name => `"${name}"`)
+        .join(', ')
+
+      throw new Error(
+        `ProgressiveFragmentMatcher: unknown strategy "${name}" (must be one of ${strategyNames}).`
+      )
+    }
+
+    const strategy = strategies[name]
+
+    // initiate type map.
+    this.possibleTypesMap = {}
+    this.match = this.match.bind(this)
+
+    // set strategy based methods.
+    this.link = strategy.link.bind(this)
   }
 
   ensureReady () {
@@ -58,8 +87,9 @@ export class ProgressiveFragmentMatcher {
       return true
     }
 
-    const implementingTypes = this.possibleTypesMap[obj.__typename]
-    if (implementingTypes && implementingTypes.indexOf(typeCondition) > -1) {
+    const possibleTypes = this.possibleTypesMap[obj.__typename]
+
+    if (possibleTypes && possibleTypes.indexOf(typeCondition) > -1) {
       return true
     }
 
--- src/fragmentMatcher.test.js
+++ src/fragmentMatcher.test.js
@@ -23,6 +23,15 @@ describe('ProgressiveFragmentMatcher', () => {
     expect(fragmentMatcher).toBeInstanceOf(ProgressiveFragmentMatcher)
   })
 
+  it('should throw when unknown strategy', () => {
+    expect(
+      () =>
+        new ProgressiveFragmentMatcher({
+          strategy: 'unkwnown'
+        })
+    ).toThrow(/unknown strategy/)
+  })
+
   it('should fetch a scalar field', async () => {
     const data = { field: 'bar' }
     const { client } = newClient({ data })
@@ -53,104 +62,106 @@ describe('ProgressiveFragmentMatcher', () => {
     expect(result).toHaveProperty('data.obj.field', 'bar')
   })
 
-  it('should send extension enabled flag', async () => {
-    const data = { field: 'bar' }
-    const { client, handler } = newClient({ data })
-    const query = gql`
-      {
-        field
-      }
-    `
+  describe('strategy: extension', () => {
+    it('should send extension enabled flag', async () => {
+      const data = { field: 'bar' }
+      const { client, handler } = newClient({ data })
+      const query = gql`
+        {
+          field
+        }
+      `
 
-    await client.query({ query })
+      await client.query({ query })
 
-    expect(handler).toHaveBeenCalledTimes(1)
+      expect(handler).toHaveBeenCalledTimes(1)
 
-    expect(handler).toHaveProperty(
-      'mock.calls.0.0.extensions.possibleTypes',
-      true
-    )
-  })
+      expect(handler).toHaveProperty(
+        'mock.calls.0.0.extensions.possibleTypes',
+        true
+      )
+    })
 
-  it('should fetch a direct type fragment', async () => {
-    const data = { obj: { __typename: 'Obj', field: 'bar' } }
-    const extensions = { possibleTypes: { Obj: ['Obj'] } }
-    const { client } = newClient({ data, extensions })
-    const query = gql`
-      {
-        obj {
-          ... on Obj {
-            field
+    it('should fetch a direct type fragment', async () => {
+      const data = { obj: { __typename: 'Obj', field: 'bar' } }
+      const extensions = { possibleTypes: { Obj: ['Obj'] } }
+      const { client } = newClient({ data, extensions })
+      const query = gql`
+        {
+          obj {
+            ... on Obj {
+              field
+            }
           }
         }
-      }
-    `
+      `
 
-    const result = await client.query({ query })
+      const result = await client.query({ query })
 
-    expect(result).toHaveProperty('data.obj.field', 'bar')
-  })
+      expect(result).toHaveProperty('data.obj.field', 'bar')
+    })
 
-  it('should fetch an inheriting fragment', async () => {
-    const data = { obj: { __typename: 'Obj', field: 'bar' } }
-    const extensions = { possibleTypes: { Obj: ['ParentType'] } }
-    const { client } = newClient({ data, extensions })
-    const query = gql`
-      {
-        obj {
-          ... on ParentType {
-            field
+    it('should fetch an inheriting fragment', async () => {
+      const data = { obj: { __typename: 'Obj', field: 'bar' } }
+      const extensions = { possibleTypes: { Obj: ['ParentType'] } }
+      const { client } = newClient({ data, extensions })
+      const query = gql`
+        {
+          obj {
+            ... on ParentType {
+              field
+            }
           }
         }
+      `
+
+      const result = await client.query({ query })
+
+      expect(result).toHaveProperty('data.obj.field', 'bar')
+    })
+
+    it('should fetch on multiple inheriting fragments', async () => {
+      const data = {
+        characters: [
+          { __typename: 'Human', name: 'Luke', height: '180' },
+          { __typename: 'Droid', name: 'R2D2', primaryFunction: 'joke' }
+        ]
       }
-    `
-
-    const result = await client.query({ query })
-
-    expect(result).toHaveProperty('data.obj.field', 'bar')
-  })
-
-  it('should fetch on multiple inheriting fragments', async () => {
-    const data = {
-      characters: [
-        { __typename: 'Human', name: 'Luke', height: '180' },
-        { __typename: 'Droid', name: 'R2D2', primaryFunction: 'joke' }
-      ]
-    }
-    const extensions = {
-      possibleTypes: {
-        Human: ['Character'],
-        Droid: ['Character']
-      }
-    }
-
-    const { client } = newClient({ data, extensions })
-    const query = gql`
-      fragment characterFields on Character {
-        name
-
-        ... on Droid {
-          primaryFunction
-        }
-
-        ... on Human {
-          height
+      const extensions = {
+        possibleTypes: {
+          Human: ['Character'],
+          Droid: ['Character']
         }
       }
 
-      query {
-        characters {
-          __typename
-          ...characterFields
+      const { client } = newClient({ data, extensions })
+      const query = gql`
+        fragment characterFields on Character {
+          name
+
+          ... on Droid {
+            primaryFunction
+          }
+
+          ... on Human {
+            height
+          }
         }
-      }
-    `
 
-    const result = await client.query({ query })
+        query {
+          characters {
+            __typename
+            ...characterFields
+          }
+        }
+      `
 
-    expect(result).toHaveProperty('data.characters.0.name', 'Luke')
-    expect(result).toHaveProperty('data.characters.0.height', '180')
-    expect(result).toHaveProperty('data.characters.1.name', 'R2D2')
-    expect(result).toHaveProperty('data.characters.1.primaryFunction', 'joke')
+      const result = await client.query({ query })
+
+      expect(result).toHaveProperty('data.characters.0.name', 'Luke')
+      expect(result).toHaveProperty('data.characters.0.height', '180')
+      expect(result).toHaveProperty('data.characters.1.name', 'R2D2')
+      expect(result).toHaveProperty('data.characters.1.primaryFunction', 'joke')
+    })
   })
 })
